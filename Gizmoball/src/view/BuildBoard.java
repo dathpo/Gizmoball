776a0d7646c9e0897114bc203ebfa84b83e8d3ff
cairns91 <cpb12159@uni.strath.ac.uk>
2017-02-06 11:11:12 +0000

    Clean up

--- Gizmoball/src/view/BuildBoard.java
+++ Gizmoball/src/view/BuildBoard.java
@@ -1,5 +1,5 @@
 package view;
 
-public class BuildBoard extends Board {
+public class BuildBoard {
 	
    }
--- Gizmoball/src/view/PlayBoard.java
+++ Gizmoball/src/view/PlayBoard.java
@@ -1,5 +1,5 @@
 package view;
 
-public class PlayBoard extends Board {
+public class PlayBoard {
    
    }
