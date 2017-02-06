bf8e35320c0d66f29885f5bb72e121a6b622082b
David Pocock <dsb13181@ds.strath.ac.uk>
2017-02-06 10:57:05 +0000

    merged Greig's two GUI classes with project packages and view interface IGUI

--- Gizmoball/src/view/BuildGUI.java
+++ Gizmoball/src/view/BuildGUI.java
@@ -1,6 +1,232 @@
 package view;
 
-public class BuildGUI implements IGUI {
-   
+import javax.swing.*;
+import java.awt.*;
+
+
+/**
+ * Created by Cairns on 01/02/2017.
+ */
+
+public class BuildGUI extends JPanel implements IGUI {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private JFrame buildFrame;
+
+    public static void main(String[] args) {
+
+        new BuildGUI();
+    }
+
+    public BuildGUI() {
+
+        BuildFrame();
+        MenuBar();
+        Mode();
+        Operations();
+        Gizmos();
+        BallEdit();
+        Board();
+        makeFrameVisible();
+    }
+
+    public void BuildFrame() {
+
+        buildFrame = new JFrame();
+        buildFrame.setTitle("Gizmoball Build Mode");
+        buildFrame.setSize(1000, 1000);
+        buildFrame.setLocationRelativeTo(null);
+        buildFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void makeFrameVisible() {
+
+        buildFrame.setVisible(true);
+    }
+
+    public void MenuBar() {
+
+        JMenuBar MenuBar = new JMenuBar();
+        buildFrame.setJMenuBar(MenuBar);
+
+        JMenu MenuOptions = new JMenu("Options");
+        MenuBar.add(MenuOptions);
+
+        JMenuItem load = new JMenuItem("Load");
+        MenuOptions.add(load);
+
+        JMenuItem Save = new JMenuItem("Save");
+        MenuOptions.add(Save);
+
+        JMenuItem Exit = new JMenuItem("Exit");
+        MenuOptions.add(Exit);
+
+    }
+
+    public void Mode() {
+
+        JPanel buttons = new JPanel();
+
+        JButton run = new JButton("Play Mode");
+        buttons.add(run);
+
+        buildFrame.getContentPane().add(buttons, BorderLayout.NORTH);
+
+    }
+
+    public void Gizmos() {
+
+        JPanel buttons = new JPanel();
+        buttons.setLayout(new GridLayout(10, 2));
+
+        JButton circle = new JButton("Circle");
+        buttons.add(circle);
+
+        JButton triangle = new JButton("Triangle");
+        buttons.add(triangle);
+
+        JButton square = new JButton("Square");
+        buttons.add(square);
+
+        JButton lFlipper = new JButton("Left Flipper");
+        buttons.add(lFlipper);
+
+        JButton rFlipper = new JButton("Right Flipper");
+        buttons.add(rFlipper);
+
+        JButton absorb = new JButton("Absorber");
+        buttons.add(absorb);
+
+        buildFrame.getContentPane().add(buttons, BorderLayout.WEST);
+    }
+
+    public void BallEdit() {
+
+        JPanel buttons = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.VERTICAL;
+        c.anchor = GridBagConstraints.NORTH;
+
+        JButton friction = new JButton("Friction");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.ipadx = 40;
+        c.ipady = 20;
+        buttons.add(friction, c);
+
+        JTextField frictionT1 = new JTextField();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.gridx = 1;
+        c.gridy = 0;
+        c.ipadx = 40;
+        c.ipady = 20;
+        buttons.add(frictionT1, c);
+
+        JTextField frictionT2 = new JTextField();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.gridx = 2;
+        c.gridy = 0;
+        c.ipadx = 40;
+        c.ipady = 20;
+        buttons.add(frictionT2, c);
+
+        JButton gravity = new JButton("Gravity");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.ipadx = 40;
+        c.ipady = 20;
+        buttons.add(gravity, c);
+
+        JTextField gravityT1 = new JTextField();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.gridx = 1;
+        c.gridy = 1;
+        c.ipadx = 40;
+        c.ipady = 20;
+        buttons.add(gravityT1, c);
+
+        JTextField gravityT2 = new JTextField();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.gridx = 2;
+        c.gridy = 1;
+        c.ipadx = 40;
+        c.ipady = 20;
+        buttons.add(gravityT2, c);
+
+        JButton addBall = new JButton("Add Ball");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.gridx = 0;
+        c.gridy = 2;
+        c.ipadx = 40;
+        c.ipady = 20;
+        buttons.add(addBall, c);
+
+        JTextField addBallT1 = new JTextField();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.gridx = 1;
+        c.gridy = 2;
+        c.ipadx = 40;
+        c.ipady = 20;
+        buttons.add(addBallT1, c);
+
+        buildFrame.getContentPane().add(buttons, BorderLayout.EAST);
+    }
+
+    public void Operations() {
+
+        JPanel operations = new JPanel();
+        operations.setLayout(new GridLayout(2, 10));
+
+        JButton move = new JButton("Move Gizmo");
+        operations.add(move);
+
+        JButton rotate = new JButton("Rotate Gizmo");
+        operations.add(rotate);
+
+        JButton delete = new JButton("Delete Gizmo");
+        operations.add(delete);
+
+        JButton clear = new JButton("Clear Board");
+        operations.add(clear);
+
+        JButton connect = new JButton("Connect Gizmo");
+        operations.add(connect);
+
+        JButton disconnect = new JButton("Disconnect Gizmo");
+        operations.add(disconnect);
+
+        JButton keyConnect = new JButton("Key Connect Gizmo");
+        operations.add(keyConnect);
+
+        JButton keyDisconnect = new JButton("Key Disconnect Gizmo");
+        operations.add(keyDisconnect);
+
+        buildFrame.getContentPane().add(operations, BorderLayout.SOUTH);
+    }
+
+    public void Board() {
+
+        JPanel board = new JPanel();
+
+        JTextArea gameBoard = new JTextArea();
+        board.add(gameBoard);
+
+        board.setBorder(BorderFactory.createLineBorder(Color.black));
+        buildFrame.getContentPane().add(board, BorderLayout.CENTER);
+
+    }
 
 }
